96934c753d357945ea5cd20ae578d5fd21fc6015
shiva kumar <98257507+Shivuduu19@users.noreply.github.com.com>
2024-07-30 12:39:22 +0530

    imporived animation

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
 
       </head>
-      <body className={GeneralRegular.className}>
+      <body className={`${GeneralRegular.className} relative`}>
         <SmoothScroller>
 
           <Navbar />
