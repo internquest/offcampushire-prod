ce1f5b0efa079c9fc12a9899019535c24adcc98c
shiva kumar <98257507+Shivuduu19@users.noreply.github.com.com>
2024-08-03 08:33:29 +0530

    adjust scroll speed

--- src/components/Landing/Marquee.jsx
+++ src/components/Landing/Marquee.jsx
@@ -16,9 +16,9 @@ const Marquee = ({ marqref }) => {
 
 
     return (
-        <section data-scroll data-scroll-speed='.35' data-scroll-offset='-100%,100%' data-scroll-class='isInview' className={`text-white opacity-0 translate-y-0 pb-6  relative bg-gradient-to-r from-[#6161A6] to-[#252540]  `}>
+        <section data-scroll data-scroll-speed='.6' data-scroll-offset='-150%,50%' data-scroll-class='isInview' className={`text-white opacity-0 translate-y-0 pb-6  relative bg-gradient-to-r from-[#6161A6] to-[#252540]  `}>
             <span className='h-[0.9rem] w-full absolute left-0 mb-[-1px] bottom-full bg-gradient-to-r from-[#6161A6] to-[#252540]  rounded-tr-full rounded-tl-full '></span>
-            <div className='w-full h-full pt-[3.13rem] lg:pt-[5.31rem]  lg:pb-[] pb-[4.6rem]  '>
+            <div className='w-full h-full pt-[3.13rem] lg:pt-[5.31rem] pb-[4.6rem]   lg:pb-[16rem]   '>
                 <div className='mx-auto   lg:max-w-[90rem] px-[1.3rem]'>
                     <div className='py-[15px] relative before:content-[""] before:top-0 before:absolute before:left-[-50%] before:right-[-50%] before:h-[1px] before:bg-current before:opacity-30  after:content-[""] after:bottom-0 after:absolute after:left-[-50%] after:right-[-50%] after:h-[1px] after:bg-current after:opacity-30  '>
                         <div className=' text-[280px] lg:text-[350px] uppercase whitespace-nowrap flex tracking-[-.015em] leading-[.73]  '>
