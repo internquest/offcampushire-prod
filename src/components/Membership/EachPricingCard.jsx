7a4421252a7955b30594904cb626e1c2176f0691
shiva kumar <98257507+Shivuduu19@users.noreply.github.com>
2024-11-24 15:27:59 +0530

    atyle improvemnet

--- src/components/Membership/EachPricingCard.jsx
+++ src/components/Membership/EachPricingCard.jsx
@@ -9,7 +9,7 @@ const EachPricingCard = () => {
                 <div></div>
             </div>
 
-            <div className={` mem1card w-full lg:max-w-[450px] xl:max-w-[676px] min-h-[411px] pr-2 -mr-28 text-white   flex justify-center items-center flex-col relative flex-grow rounded-[20px] ${GeneralMedium.className}`}>
+            <div className={` mem1card w-full lg:max-w-[450px] xl:max-w-[676px] min-h-[411px] pr-2 lg:-mr-28 text-white   flex justify-center items-center flex-col relative flex-grow rounded-[20px] ${GeneralMedium.className}`}>
                 <div className="bg-white w-[70%]  h-[250px] lg:w-[300px] xl:h-[256px] xl:w-[352px]  lg:block hidden absolute bottom-[100%] lg:bottom-0 left-[-40%] rounded-t-[10px] lg:-mr-[80px] z-10 border border-[#D7D7D7] overflow-hidden">
                     <div className="w-full h-[70px] bg-[#ECE9FF] "></div>
                     <div></div>
